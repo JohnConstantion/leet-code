1e66a0dc4448d69ad8430c76babd655aa659d8e6
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2022-11-15 16:37:11 +0800

    Initial commit

---
+++ src/main/java/com/learn/leetcode/LetCodeApplication.java
@@ -0,0 +1,16 @@
+package com.learn.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author johnconstantine
+ */
+@SpringBootApplication
+public class LeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeApplication.class, args);
+    }
+
+}
